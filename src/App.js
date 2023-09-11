79e258290916ca040daf28430facf4c3b3c12588
¨David <oflores@ingenieria.digital>
2023-09-11 16:09:00 -0600

    prueba3

--- src/App.js
+++ src/App.js
@@ -1,30 +1,8 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
 const App = () => {
   return (
     <div className="App">
       <h1>Test</h1>
->>>>>>> 191614b505608d5744ed9a2c1b3b6a206c196aae
     </div>
   );
 }
