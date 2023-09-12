3f994bf7a1d689b8dedb89463b48581e3cf5f431
¨David <oflores@ingenieria.digital>
2023-09-12 09:46:18 -0600

    Probar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-import Login from "./components/pages/Login";
-const App = () => {
-  return (
-    <div className="App">
-      <Login />
-=======
+
 import React, { useContext } from "react";
 import { Link, Outlet } from "react-router-dom";
 
@@ -12,7 +6,6 @@ const App = () => {
   return (
     <div>
       <Outlet />
->>>>>>> aaed58ee93f4c027bbe37fc96bbd8e30a13589cc
     </div>
   );
 };
